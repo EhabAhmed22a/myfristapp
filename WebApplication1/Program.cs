81fad9f3b82464f5489e341204a17ca974f18590
Ehab <2100632@eng.asu.edu.eg>
2025-09-19 17:28:17 +0300

    change the swagger conf

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -10,11 +10,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline2.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
